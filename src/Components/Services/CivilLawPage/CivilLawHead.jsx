3f0b3066d2365d55fb1ad5e772ddc86455497053
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-07-23 19:10:21 +0530

    litigation

--- src/Components/Services/CivilLawPage/CivilLawHead.jsx
+++ src/Components/Services/CivilLawPage/CivilLawHead.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./CivilLawPage.css";
 import civilLawImg from "./../../../Images/ServicesImg/civilLaw.png";
+import civilLawImgMob from "./../../../Images/ServicesImg/civilLawMob.png";
+
 const CivilLawHead = () => {
   return (
     <div className="civilLawHeadContainer">
-      <img src={civilLawImg} alt="civilLawImg" />
+      <img className="imgComp" src={civilLawImg} alt="civilLawImg" />
+      {/* <img className="imgMob" src={civilLawImgMob} alt="civilLawImgMob" /> */}
+
       <div className="civilLawHeadCover"></div>
       <h2>Civil Law</h2>
     </div>
